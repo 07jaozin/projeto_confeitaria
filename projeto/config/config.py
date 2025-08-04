8a632e9016263d2f4b55ab07b454cfbc6870547e
joao victor valentim <joaovictorvalentim.m@gmail.com>
2025-08-04 20:57:44 -0300

    imgs

--- projeto/config/config.py
+++ projeto/config/config.py
@@ -7,9 +7,9 @@ import secrets
 BASE_DIR = os.getcwd()
 
 class Config:
-    #SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI =  'sqlite:///database.db'
-    UPLOAD_FOLDER = 'static/img'
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI =  'sqlite:///database.db'
+    #UPLOAD_FOLDER = 'static/img'
     SECRET_KEY = secrets.token_hex(32)
     PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=900000)
     cloudinary.config(
